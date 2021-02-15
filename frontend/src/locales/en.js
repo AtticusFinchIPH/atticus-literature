493d47ab96c441d1ecb868426154ff367781402c
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-02-15 17:01:07 +0100

    Translate for Catalogue

--- frontend/src/locales/en.js
+++ frontend/src/locales/en.js
@@ -34,6 +34,12 @@ const en = defineMessages({
     shipping_policy: `Free delivery within Hanoi & Ho Chi Minh City in maximum 2 hours.
     Delivery within a maximum of 1 week across the territory of Vietnam.`,
     return_policy: `If you are not satisfied with a product you ordered from our store or if it is defective or damaged, you can return it to us within 3 days of the delivery date, and we will refund you or will replace the entire item.`,
+
+    all_categories: 'All Categories',
+    vi_literature: 'Vietnamese Literature',
+    asian_literature: 'Asian Literature',
+    western_literature: 'Western Literature',
+    genres: 'Genres',
 })
 
 export default en;
