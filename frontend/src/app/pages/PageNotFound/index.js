f3eb3e7e2947151e37dd74f351ead3aab166b0ac
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-02-16 18:17:43 +0100

    Add link to home  to  PageNotFound

--- frontend/src/app/pages/PageNotFound/index.js
+++ frontend/src/app/pages/PageNotFound/index.js
@@ -1,12 +1,13 @@
-import { Hidden } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import error_desktop from '../../../images/404_desktop.jpg';
-import error_mobile from '../../../images/404_mobile.jpg';
+import { Link } from 'react-router-dom';
+import error_image from '../../../images/404.jpg';
 
 const useStyle = makeStyles((theme) => ({
     root: {
         position: 'relative',
         top: theme.spacing(7),
+        paddingBottom: theme.spacing(1.5),
         backgroundColor: theme.palette.background.main,
         height: '90vh',
         textAlign: 'center',
@@ -14,37 +15,65 @@ const useStyle = makeStyles((theme) => ({
             top: theme.spacing(8),
         },
     },
-    errorImage: {
-        backgroundImage: `url(${error_desktop})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        height: '85vh',
-        marginBottom: theme.spacing(1),
+    container: {
+        position: 'relative',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        height: '100%',
+        overflow: 'hidden',
+        backgroundColor: '#4cbdcb',
+    },
+    img: {
+        height: '576px',
+        width: '960px',
         [theme.breakpoints.down('sm')]: {
-            backgroundImage: `url(${error_mobile})`,
+            height: '516px',
+            width: '860px',
+        },
+        [theme.breakpoints.up('lg')]: {
+            height: '768px',
+            width: '1280px',
         },
     },
+    linkHome: {
+        position: 'absolute',
+        transform: 'translateY(346px)',
+        height: '54px',
+        width: '240px',
+        [theme.breakpoints.down('sm')]: {
+            transform: 'translateY(312px)',
+            height: '46px',
+            width: '220px',
+        },
+        [theme.breakpoints.up('lg')]: {
+            transform: 'translateY(468px)',
+            height: '62px',
+            width: '312px',
+        },
+    },
+    iconButton: {
+        height: '100%', width: '100%', borderRadius: '50px',
+    },
     attribute: {
-        color: theme.palette.text.main,
-    }
+        color: '#fff',
+        textDecoration: 'none',
+    },
 }));
 
 const PageNotFound = () => {
     const classes = useStyle();
     return(
         <div className={classes.root}>
-            <div className={classes.errorImage}/>
-            <Hidden mdUp>
-            <a className={classes.attribute} href="http://www.freepik.com" target='_blank'>
-                Image above is designed by Bamdewanto / Freepik
-            </a>
-            </Hidden>
-            <Hidden smDown>
-            <a className={classes.attribute} href="http://www.freepik.com" target='_blank'>
-                Image above is designed by roserodionova / Freepik
-            </a>
-            </Hidden>
+            <div className={classes.container}>
+                <img src={error_image} className={classes.img}/>
+                <a className={classes.attribute} href="http://www.freepik.com" target='_blank'>
+                    Image designed by roserodionova / Freepik
+                </a>
+                <Link to='/' className={classes.linkHome}>
+                    <IconButton className={classes.iconButton}/>
+                </Link>
+            </div>
         </div>
     )
 }
