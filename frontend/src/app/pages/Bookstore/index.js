8f8cf0564e2cc4261c39c4123ad5a5b82c485e4b
AtticusFinchIPH <tranvan.duc.012@gmail.com>
2021-02-16 12:44:10 +0100

    Build list in Bookstore without Pagination

--- frontend/src/app/pages/Bookstore/index.js
+++ frontend/src/app/pages/Bookstore/index.js
@@ -1,12 +1,90 @@
-import { useSelector } from 'react-redux';
+import { useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
-import { Container, Typography } from '@material-ui/core';
+import { Box, Card, CardActions, CardContent, CardMedia, Container, Fade, IconButton, Typography } from '@material-ui/core';
 import useStyle from './styles';
+import CartOpenContext from '../../../contexts/CartOpenContext';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
 import Catalogue from './Catalogue';
+import { addToCart } from '../../../actions/productActions';
+
+const CardItem = (props) => {
+    const classes = useStyle();
+    const [isShown, setIsShown] = useState(false);
+    const { setCartOpen } = useContext(CartOpenContext);
+    const dispatch = useDispatch();
+    const item = props.item;
+    let priceDeclare;
+    switch (item.currency) {
+        case 'usd':
+            priceDeclare = `$ ${item.price}`;
+            break;
+        case 'vnd':
+            priceDeclare = `${item.price} vnđ`;
+            break;
+        default:
+            priceDeclare = `$ ${item.price}`;
+            break;
+    }
+    const redirect = () => {
+        console.log(item);
+    }
+    const addItemToCart = () => {
+        dispatch(addToCart(item));
+        setCartOpen(true);
+    } 
+    return(
+        <>
+        <Card className={classes.cardItem} >
+            <CardMedia className={classes.cardMedia} image={item.image} title={item.title} 
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
+                onClick={redirect}
+            />
+            <CardContent className={classes.cardContent}>
+                <Typography variant='body1' component='p'>{item.title}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <IconButton className={classes.iconButton} aria-label="Add to favorites">
+                    <FavoriteBorderOutlinedIcon className={classes.icon}/>
+                </IconButton>
+                <IconButton className={classes.iconButton} aria-label="Add to cart" onClick={addItemToCart}>
+                    <AddShoppingCartIcon className={classes.icon} />
+                </IconButton>
+            </CardActions>
+        </Card>
+        <Fade in={isShown} timeout={{appear: 100, enter: 300, exit: 100}} unmountOnExit>
+            <Box className={classes.quickView} 
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
+                onClick={redirect}
+            >
+                <Typography variant='body1'>
+                    {priceDeclare}
+                </Typography>
+            </Box>       
+        </Fade>
+        </>
+    )
+}
+
+CardItem.propTypes = {
+    item: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+        price: PropTypes.number.isRequired,
+    })
+}
 
 const Bookstore = () => {
     const classes = useStyle();
     const language = useSelector(state => state.language);
+    const { products } = useSelector(state => state.store);
+    console.log(products)
     return (
         <div className={classes.root}>
             <Container className={classes.container} maxWidth='xl'>
@@ -15,21 +93,13 @@ const Bookstore = () => {
                     language === 'vi'
                     ?
                     <>
-                    <Typography variant='h3' component='h1'>
-                        Hiệu sách
-                    </Typography>
-                    <Typography variant='h5' component='h1'>
-                        của Atticus
-                    </Typography>
+                    <Typography variant='h3' component='h1'>Hiệu sách</Typography>
+                    <Typography variant='h5' component='h1'>của Atticus</Typography>
                     </>
                     :
                     <>
-                    <Typography variant='h5' component='h1'>
-                        Atticus's
-                    </Typography>
-                    <Typography variant='h3' component='h1'>
-                        Bookstore
-                    </Typography>
+                    <Typography variant='h5' component='h1'>Atticus's</Typography>
+                    <Typography variant='h3' component='h1'>Bookstore</Typography>
                     </>
                 }
                 </div>
@@ -38,7 +108,15 @@ const Bookstore = () => {
                         <Catalogue />
                     </div>
                     <div className={classes.products}>
-                        List
+                        <div className={classes.table}>
+                            {
+                                products.map((item, i) => (
+                                    <div className={classes.item}>
+                                        <CardItem key={i} item={item}/>
+                                    </div>
+                                ))
+                            }
+                        </div>
                     </div>
                 </div>
             </Container>
--- frontend/src/app/pages/Bookstore/styles.js
+++ frontend/src/app/pages/Bookstore/styles.js
@@ -1,5 +1,5 @@
 import { makeStyles } from '@material-ui/styles';
-import { } from '../../../utils/theme';
+import { FONT_F_PLAYFAIR, LIGHT_WHITE } from '../../../utils/theme';
 
 const useStyle = makeStyles((theme) => ({
     root: {
@@ -23,7 +23,8 @@ const useStyle = makeStyles((theme) => ({
     },
     content: {
         display: 'grid',
-        gridTemplateColumns: '300px auto',
+        gridTemplateColumns: '280px auto',
+        gridGap: theme.spacing(5),
         [theme.breakpoints.down('sm')]: {
             display: 'flex',
         },
@@ -35,6 +36,70 @@ const useStyle = makeStyles((theme) => ({
     },
     products: {
         width: '100%',
+    },
+    table: {
+        
+    },
+    item: {
+        display: 'inline-block',
+        position: 'relative',
+        height: theme.spacing(50),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '33.33%',
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: '20%',
+        },
+    },
+    cardItem: {
+        position: 'absolute',
+        left: '50%',
+        transform: 'translateX(-50%)',
+        height: theme.spacing(50),
+        width: theme.spacing(21),
+        fontFamily: FONT_F_PLAYFAIR,
+        color: theme.palette.text.main,
+        background: 'none',
+        boxShadow: 'none',
+        cursor: 'pointer',
+        transition: 'all 150ms ease-in-out',
+        '&:hover': {
+            transform: 'translateX(-50%) scale(1.05)',
+        },
+    },
+    cardMedia: {
+        height: theme.spacing(31.5),
+        width: theme.spacing(21), 
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center', 
+    },
+    cardContent: {
+        height: theme.spacing(8),
+        paddingBottom: '0',
+        overflow: 'hidden',
+    },
+    cardActions: {
+        display: 'block',
+    },
+    iconButton: {
+        
+    },
+    icon: {
+        color: theme.palette.text.main,
+    },
+    quickView: {
+        position: 'absolute',
+        left: '50%',
+        bottom: theme.spacing(18),
+        height: theme.spacing(5),
+        padding: theme.spacing(1),
+        background: 'rgba(255, 255, 255, 0.75)',
+        width: '168px', // CARD_ITEM_WIDTH - padding*2
+        cursor: 'pointer',
+        color: '#000',
+        transform: 'translateX(-50%) scale(1.05)',
     },
 }));
 
